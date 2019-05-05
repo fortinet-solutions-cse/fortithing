379b396dbafc26ffe98def90f6415f8358b839a6
Miguel Angel Muñoz <magonzalez@fortinet.com>
2019-05-05 12:43:20 +0200

    WIP: FortiThing Library - Draft

--- micropython/full_features_example.py
+++ micropython/full_features_example.py
@@ -1,7 +1,6 @@
-import bme280_float, neopixel, network, ssd1306
-from simple import MQTTClient
 from time import sleep
-from machine import Pin, I2C, ADC
+
+from fortithing import FortiThing
 
 server = "xxxxx.messaging.internetofthings.ibmcloud.com"
 client_id = "d:xxxxx:BME280:mynewdevice"
@@ -13,58 +12,33 @@ password = "xxxxxxxx"
 wifi_essid = 'xxxxxx'
 wifi_password = 'xxxxxx'
 
-i2c = I2C(scl=Pin(5), sda=Pin(4), freq=400000)
-bme = bme280_float.BME280(i2c=i2c)
-adc = ADC(0)
-np = neopixel.NeoPixel(Pin(14), 2)
-blueled = Pin(16, Pin.OUT)
-blueled.off()
-oled = ssd1306.SSD1306_I2C(128, 64, i2c, 0x3c)
-c = None
-
-
-def screen(*argv):
-    oled.fill(0)
-    oled.text(argv[0], 0, 0)
-
-    index = 0
-    for arg in argv[1:]:
-        oled.text(arg, 0, index*12 + 20)
-        index += 1
-    oled.show()
-
+ft = FortiThing()
 
 def sub_cb(topic, msg):
     print((topic, msg))
     if 'switch_rgb_lights' in topic:
         light_value = int(msg) * 255
-        np[0] = (light_value, light_value, light_value)
-        np[1] = (light_value, light_value, light_value)
-        np.write()
+        ft.set_left_rgb_light(light_value, light_value, light_value)
+        ft.set_right_rgb_light(light_value, light_value, light_value)
     elif 'set_left_rgb_light_color' in topic:
         value = msg.decode('utf-8').lstrip('#')
         lv = len(value)
         r, g, b = tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
-        np[0] = (r, g, b)
-        np.write()
+        ft.set_left_rgb_light(r, g, b)
     elif 'set_right_rgb_light_color' in topic:
         value = msg.decode('utf-8').lstrip('#')
         lv = len(value)
         r, g, b = tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
-        np[1] = (r, g, b)
-        np.write()
+        ft.set_right_rgb_light(r, g, b)
     elif 'set_led' in topic:
         value = int(msg)
-        if value >= 1:
-            blueled.on()
-        else:
-            blueled.off()
+        ft.set_led(value)
 
 
 def connect():
     try:
         print("Connecting WiFi and MQTT Client")
-        screen("Status", "Connecting WiFi", "and MQTT Client")
+        ft.screen("Status", "Connecting WiFi", "and MQTT Client")
         sta_if = network.WLAN(network.STA_IF)
         ap_if = network.WLAN(network.AP_IF)
 
@@ -83,15 +57,15 @@ def connect():
             screen("Error", "WiFi connection failed", "More than 60 attemps")
             raise Exception
 
-        global c
-        c = MQTTClient(client_id=client_id, server=server, port=port, ssl=ssl, user=user, password=password)
-        c.set_callback(sub_cb)
-        c.connect()
+        global mqttClient
+        mqttClient = MQTTClient(client_id=client_id, server=server, port=port, ssl=ssl, user=user, password=password)
+        mqttClient.set_callback(sub_cb)
+        mqttClient.connect()
 
-        c.subscribe("iot-2/cmd/switch_rgb_lights/fmt/json")
-        c.subscribe("iot-2/cmd/set_left_rgb_light_color/fmt/json")
-        c.subscribe("iot-2/cmd/set_right_rgb_light_color/fmt/json")
-        c.subscribe("iot-2/cmd/set_led/fmt/json")
+        mqttClient.subscribe("iot-2/cmd/switch_rgb_lights/fmt/json")
+        mqttClient.subscribe("iot-2/cmd/set_left_rgb_light_color/fmt/json")
+        mqttClient.subscribe("iot-2/cmd/set_right_rgb_light_color/fmt/json")
+        mqttClient.subscribe("iot-2/cmd/set_led/fmt/json")
     except:
         print("Exception occurred when connecting.")
         screen("Error", "Exception occurred", "when connecting")
@@ -102,16 +76,16 @@ def loop():
     button = False
     try:
         while 1:
-            global c
-            c.check_msg()
+            global mqttClient
+            mqttClient.check_msg()
             sleep(0.1)
 
             if button is False and Pin(13, Pin.IN, Pin.PULL_UP).value():
                 button = True
-                c.publish("iot-2/evt/switch/fmt/json", str(not button))
+                mqttClient.publish("iot-2/evt/switch/fmt/json", str(not button))
             elif button is True and not Pin(13, Pin.IN, Pin.PULL_UP).value():
                 button = False
-                c.publish("iot-2/evt/switch/fmt/json", str(not button))
+                mqttClient.publish("iot-2/evt/switch/fmt/json", str(not button))
 
             if count == 30:
                 result = bme.read_compensated_data()
@@ -119,10 +93,10 @@ def loop():
                 p = result[1]
                 h = result[2]
                 print(bme.values)
-                c.publish("iot-2/evt/temp/fmt/json", '{{ "t":{:.2f} }}'.format(t))
-                c.publish("iot-2/evt/pressure/fmt/json", '{{ "p":{:.2f} }}'.format(p/100))
-                c.publish("iot-2/evt/humidity/fmt/json", '{{ "h":{:.2f} }}'.format(h))
-                c.publish("iot-2/evt/light_sensor/fmt/json", '{{ "l":{:.2f} }}'.format(adc.read()))
+                mqttClient.publish("iot-2/evt/temp/fmt/json", '{{ "t":{:.2f} }}'.format(t))
+                mqttClient.publish("iot-2/evt/pressure/fmt/json", '{{ "p":{:.2f} }}'.format(p/100))
+                mqttClient.publish("iot-2/evt/humidity/fmt/json", '{{ "h":{:.2f} }}'.format(h))
+                mqttClient.publish("iot-2/evt/light_sensor/fmt/json", '{{ "l":{:.2f} }}'.format(adc.read()))
                 screen("Weather data", "Temp: {:.2f} C".format(t),
                        "Pres: {:.2f} hPa".format(p/100),
                        "Humi: {:.2f} %".format(h))
